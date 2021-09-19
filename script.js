3bd758038370edbd212690e0c6a80aedffb653ac
srv-pranjal <prns366@gmail.com>
2021-09-19 12:50:38 +0530

    docs: added styling

--- script.js
+++ script.js
@@ -1,13 +1,16 @@
 const inputs = document.querySelectorAll(".input");
 const outputMessage = document.querySelector("#output");
 const formID = document.querySelector("#form");
+const sadImg = document.querySelector("#sadimg");
+const happyImg = document.querySelector("#happyimg");
 
 let isUserInLoss = false;
 function calculateProfitOrLoss() {
+  isUserInLoss = false;
   const initialPrice = Number(inputs[0].value);
   const quantity = Number(inputs[1].value);
   const currentPrice = Number(inputs[2].value);
-  const profitOrLoss = (currentPrice - initialPrice) * quantity;
+  let profitOrLoss = (currentPrice - initialPrice) * quantity;
   if (profitOrLoss < 0) {
     isUserInLoss = true;
     profitOrLoss = 0 - profitOrLoss;
@@ -18,6 +21,8 @@ function calculateProfitOrLoss() {
 }
 
 function formSubmitHandler(e) {
+  sadImg.style.display = "none";
+  happyImg.style.display = "none";
   e.preventDefault();
   let [profitOrLoss, profitOrLossPercentage] = calculateProfitOrLoss();
   if (!Number.isInteger(profitOrLoss)) {
@@ -30,9 +35,11 @@ function formSubmitHandler(e) {
     outputMessage.innerText = "You don't have any profit or loss yet";
   } else {
     if (isUserInLoss) {
-      outputMessage.innerText = `Sorry to say but you are in a loss of ₹${profitOrLoss} bearing a loss percentage of ${profitOrLossPercentage}%`;
+      outputMessage.innerText = `Sorry to say but you are in a loss of ₹${profitOrLoss} bearing a loss percentage of ${profitOrLossPercentage}% 😞`;
+      if (profitOrLossPercentage > 50) sadImg.style.display = "block";
     } else {
-      outputMessage.innerText = `Wohooo!! You are in a total profit of ₹${profitOrLoss} bearing a profit percentage of ${profitOrLossPercentage}%`;
+      outputMessage.innerText = `Wohooo!!🥳🥳 You are in a total profit of ₹${profitOrLoss} bearing a profit percentage of ${profitOrLossPercentage}%`;
+      if (profitOrLossPercentage > 50) happyImg.style.display = "block";
     }
   }
 }
